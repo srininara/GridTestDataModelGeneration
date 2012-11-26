d306988e37f721a4e2eed40c73bc21f23845ae2a
Srinivas Narayanan <srininara@gmail.com>
2012-11-26 18:17:06 +0530

    Simple Test for checking generation works through the graph of object

---
+++ src/test/java/com/nacnez/projects/grid/modelCreator/test/DataCreatorTest.java
@@ -0,0 +1,30 @@
+package com.nacnez.projects.grid.modelCreator.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import org.junit.Test;
+
+import com.nacnez.projects.grid.model.Person;
+import com.nacnez.projects.grid.modelCreator.DataCreator;
+
+public class DataCreatorTest {
+
+	private static final int TEST_COUNT = 10;
+
+	@Test
+	public void generatesDataForTheEntireObjectGraph() {
+		Collection<Person> persons = DataCreator.createData(TEST_COUNT);
+		assertEquals(TEST_COUNT,persons.size());
+		for (Person person : persons) {
+			assertNotNull(person);
+			assertNotNull(person.getFirstName());
+			assertNotNull(person.getAddress());
+			assertNotNull(person.getAddress().getFirstLine());
+			assertNotNull(person.getAddress().getCurrentLocation());
+			assertNotNull(person.getAddress().getCurrentLocation().getLatitude());
+		}
+	}
+
+}
